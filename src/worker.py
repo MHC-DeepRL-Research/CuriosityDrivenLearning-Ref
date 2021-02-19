6c81e186e6f50e7edff50b57dfe8b5284b0d933a
Melody <melodysu83@gmail.com>
2021-02-19 10:05:53 -0500

    addd comments to worker.py

--- src/worker.py
+++ src/worker.py
@@ -23,11 +23,13 @@ class FastSaver(tf.train.Saver):
                                     meta_graph_suffix, False)
 
 def run(args, server):
+    # step1: creates an RL environment
     env = create_env(args.env_id, client_id=str(args.task), remotes=args.remotes, envWrap=args.envWrap, designHead=args.designHead,
                         noLifeReward=args.noLifeReward)
+    # step2: creates an A3C trainer object
     trainer = A3C(env, args.task, args.visualise, args.unsup, args.envWrap, args.designHead, args.noReward)
 
-    # logging
+    # step3: logging hyperparameter settings to the log.txt file
     if args.task == 0:
         with open(args.log_dir + '/log.txt', 'w') as fid:
             for key, val in constants.items():
@@ -36,26 +38,34 @@ def run(args, server):
             fid.write('input observation: %s\n'%str(env.observation_space.shape))
             fid.write('env name: %s\n'%str(env.spec.id))
             fid.write('unsup method type: %s\n'%str(args.unsup))
-
-    # Variable names that start with "local" are not saved in checkpoints.
+    
+    # step4: creates tensorflow variable initializer object
+    #        use_tf12_api is a flag that checks tf version (because syntaxes are different)
+    #        variable names that start with "local" are not saved in checkpoints.
     if use_tf12_api:
         variables_to_save = [v for v in tf.global_variables() if not v.name.startswith("local")]
-        init_op = tf.variables_initializer(variables_to_save)
-        init_all_op = tf.global_variables_initializer()
+        init_op = tf.variables_initializer(variables_to_save)  # used in line 99 (updates periodically)
+        init_all_op = tf.global_variables_initializer()        # used in line 71 (only updates at the start)
     else:
         variables_to_save = [v for v in tf.all_variables() if not v.name.startswith("local")]
         init_op = tf.initialize_variables(variables_to_save)
         init_all_op = tf.initialize_all_variables()
-    saver = FastSaver(variables_to_save)
+        
+    # step5: creates an efficient parameter saver object
+    saver = FastSaver(variables_to_save)   # used in line 97
+    
+    # step6: load pretrained parameters (if any)
     if args.pretrain is not None:
         variables_to_restore = [v for v in tf.trainable_variables() if not v.name.startswith("local")]
         pretrain_saver = FastSaver(variables_to_restore)
 
+    # step7: logging all trainable parameters to logger (TODO: figure out where this goes)
     var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
     logger.info('Trainable vars:')
     for v in var_list:
         logger.info('  %s %s', v.name, v.get_shape())
 
+    # step8: creates an initialization function (used in line 100)
     def init_fn(ses):
         logger.info("Initializing all parameters.")
         ses.run(init_all_op)
@@ -66,7 +76,10 @@ def run(args, server):
             pretrain_saver.restore(ses, pretrain)
             logger.info("==> Done restoring model! Restored %d variables.", len(variables_to_restore))
 
+    # step9: training configuration set up (used in line 114)
     config = tf.ConfigProto(device_filters=["/job:ps", "/job:worker/task:{}/cpu:0".format(args.task)])
+    
+    # step10: defines a log directory (used in line 96) and a summary writer (used in line 101)
     logdir = os.path.join(args.log_dir, 'train')
 
     if use_tf12_api:
@@ -75,7 +88,11 @@ def run(args, server):
         summary_writer = tf.train.SummaryWriter(logdir + "_%d" % args.task)
 
     logger.info("Events directory: %s_%s", logdir, args.task)
-    sv = tf.train.Supervisor(is_chief=(args.task == 0),
+    
+    # step11: creates a tensorflow train supervisor object
+    #         here is more information about train supervisor in tensorflow:
+    #         https://haosdent.gitbooks.io/tensorflow-document/content/how_tos/supervisor/
+    sv = tf.train.Supervisor(is_chief=(args.task == 0),  # (TODO: figure out what this does)
                              logdir=logdir,
                              saver=saver,
                              summary_op=None,
@@ -92,19 +109,25 @@ def run(args, server):
     logger.info(
         "Starting session. If this hangs, we're mostly likely waiting to connect to the parameter server. " +
         "One common cause is that the parameter server DNS name isn't resolving yet, or is misspecified.")
+    
+    # step12: runs the tensorflow train supervisor
     with sv.managed_session(server.target, config=config) as sess, sess.as_default():
-        # Workaround for FailedPreconditionError
-        # see: https://github.com/openai/universe-starter-agent/issues/44 and 31
-        sess.run(trainer.sync)
-
+        
+        # step12-1: starts running the train supervisor (TODO: figure out what does sync do)
+        sess.run(trainer.sync)   # workaround for FailedPreconditionError
+                                 # # see: https://github.com/openai/universe-starter-agent/issues/44 and 31
+            
+        # step12-2: starts the trainer and takes the first training step
         trainer.start(sess, summary_writer)
         global_step = sess.run(trainer.global_step)
         logger.info("Starting training at gobal_step=%d", global_step)
+        
+        # step12-3: keeps running more trainingn steps until stop condition satisfies
         while not sv.should_stop() and (not num_global_steps or global_step < num_global_steps):
             trainer.process(sess)
             global_step = sess.run(trainer.global_step)
 
-    # Ask for all the services to stop.
+    # step13: ask for all the services to stop.
     sv.stop()
     logger.info('reached %s steps. worker stopped.', global_step)
 
@@ -130,9 +153,9 @@ More tensorflow setup for data parallelism
 
 def main(_):
     """
-Setting up Tensorflow for data parallel work
-"""
-
+    Setting up Tensorflow for data parallel work
+    """
+    # the parameters
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('-v', '--verbose', action='count', dest='verbosity', default=0, help='Set verbosity.')
     parser.add_argument('--task', default=0, type=int, help='Task index')
@@ -160,9 +183,11 @@ Setting up Tensorflow for data parallel work
     parser.add_argument('--pretrain', type=str, default=None, help="Checkpoint dir (generally ..../train/) to load from.")
     args = parser.parse_args()
 
+    # set up cluster parameters
     spec = cluster_spec(args.num_workers, 1, args.psPort)
     cluster = tf.train.ClusterSpec(spec).as_cluster_def()
 
+    # set up signals to stop the process
     def shutdown(signal, frame):
         logger.warn('Received signal %s: exiting', signal)
         sys.exit(128+signal)
@@ -170,6 +195,7 @@ Setting up Tensorflow for data parallel work
     signal.signal(signal.SIGINT, shutdown)
     signal.signal(signal.SIGTERM, shutdown)
 
+    # create train server for workers
     if args.job_name == "worker":
         server = tf.train.Server(cluster, job_name="worker", task_index=args.task,
                                  config=tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=2))
@@ -178,6 +204,8 @@ Setting up Tensorflow for data parallel work
             time.sleep(args.delay)
             print('.. wait over !')
         run(args, server)
+        
+    # create train server for parameter server
     else:
         server = tf.train.Server(cluster, job_name="ps", task_index=args.task,
                                  config=tf.ConfigProto(device_filters=["/job:ps"]))
